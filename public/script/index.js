d26ed813ee86d1328d736516ed665caa6ddca6c0
lifeofladi <dev@lifeofladi.com>
2021-07-22 22:39:24 -0700

    completed fron-end form validation

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,30 @@
 console.log("connected!!");
 
 $(document).ready(function () {
-  $("#newsletter").validate();
+  //sets a custom email regex pattern.
+  $.validator.methods.email = function (value, element) {
+    return (
+      this.optional(element) ||
+      /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value)
+    );
+  };
+
+  //stops numeric char in text-type inputs.
+  $("input[type='text']").keypress(function () {
+    return /[a-z]/i.test(event.key);
+  });
+
+  //validation rules
+  $("#newsletter").validate({
+    rules: {
+      fname: {
+        minlength: 2,
+        maxlength: 20,
+      },
+      lname: {
+        minlength: 2,
+        maxlength: 20,
+      },
+    },
+  });
 });
