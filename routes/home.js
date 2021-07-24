60695fc6d9b7d15fee96656d5688e6d578b92987
lifeofladi <dev@lifeofladi.com>
2021-07-23 21:13:51 -0700

    refactored home routes

---
+++ routes/home.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mailchimpAPI = require("../mailchimp-api");
+
+const router = express.Router();
+
+let statusMessage = "";
+let totalContacts = 0;
+
+//Handling GET request on home route
+router.get("/", async (req, res) => {
+  totalContacts = await mailchimpAPI.getTotalContacts();
+
+  res.render("index", { message: statusMessage, listTotal: totalContacts });
+});
+
+//Handling POST request from home route
+router.post("/", async (req, res) => {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  try {
+    let status = await mailchimpAPI.addContactToList(
+      firstName,
+      lastName,
+      email,
+    );
+
+    if (status.isDelivered !== false) {
+      statusMessage = `Thank you ${firstName}, we will be in touch!`;
+      totalContacts = status.numOfContacts;
+    } else {
+      if ("errorMsg" in status) {
+        statusMessage = status.errorMsg;
+      }
+    }
+    res.redirect("/");
+  } catch (error) {
+    console.log(error.message, "in post");
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,50 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const mailchimpAPI = require("./mailchimp-api");
-let statusMessage = "";
-let totalContacts = 0;
+const home = require("./routes/home");
 
 //setting templating engine to EJS
 app.set("view engine", "ejs");
 
-//Allows you to grab values from inputs
+//Allows you to parse url encoded values
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-//Handling GET request on home route
-app.get("/", async (req, res) => {
-  totalContacts = await mailchimpAPI.getTotalContacts();
-
-  res.render("index", { message: statusMessage, listTotal: totalContacts });
-});
-
-//Handling POST request from home route
-app.post("/", async (req, res) => {
-  const firstName = req.body.fname;
-  const lastName = req.body.lname;
-  const email = req.body.email;
-  try {
-    let status = await mailchimpAPI.addContactToList(
-      firstName,
-      lastName,
-      email,
-    );
-
-    if (status.isDelivered !== false) {
-      statusMessage = `Thank you ${firstName}, we will be in touch!`;
-      totalContacts = status.numOfContacts;
-    } else {
-      if ("errorMsg" in status) {
-        statusMessage = status.errorMsg;
-      }
-    }
-    res.redirect("/");
-  } catch (error) {
-    console.log(error.message, "in post");
-  }
-});
+app.use("/", home);
 
 app.listen(3300, () => {
   console.log("listening on port 3300");
